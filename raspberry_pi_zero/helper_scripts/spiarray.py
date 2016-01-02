5c19aac49ab822a1b7ee67c24854c987a26830aa
Johannes Natter <redrocket@gmx.at>
2016-01-02 01:12:15 +0100

    rr_base: add spiarray.py helper script

---
+++ raspberry_pi_zero/helper_scripts/spiarray.py
@@ -0,0 +1,22 @@
+
+import sys
+import time
+import spidev
+
+spi = spidev.SpiDev()
+spi.open(0, 1)
+spi.max_speed_hz=10000000
+
+argc = len(sys.argv)
+
+if argc < 2:
+	print "Error: No arguments specified"
+	print ""
+	print "Usage: spitest.py <hex-bytearray>"
+	sys.exit(1)
+
+data = [int(x, 16) for x in sys.argv[1:]]
+
+resp = spi.xfer2(data)
+resp_hex = str(bytearray(resp)).encode('hex')
+print resp_hex
