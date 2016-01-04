ebcf9bec8e225cc6c070bd6fc9b78897e8acbfce
Johannes Natter <redrocket@gmx.at>
2016-01-04 02:10:05 +0100

    rr_base: update download script

--- de0_nano/download.sh
+++ de0_nano/download.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-quartus_pgm --mode=jtag -o p\;project/output_files/tbd_rr_base.sof
+#quartus_pgm --mode=jtag -o p\;project/output_files/tbd_rr_base.sof
+scp output/rr_base.bin root@$1:/root/rr_base.bin
