a74cb7e74abda4ba92c285b4305a7003d3839e4d
Johannes Natter <notset@notset.com>
2016-11-15 21:02:16 +0100

    rr_base: add helper script

---
+++ raspberry_pi_zero/hats/eepromutils/create-rr_base-eep.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "./eepmake eeprom_settings.txt rr_base.eep ../../dts/overlays/rr_base-overlay.dtb"
+./eepmake eeprom_settings.txt rr_base.eep ../../dts/overlays/rr_base-overlay.dtb
