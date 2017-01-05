c5917c5174343bcddad800d7c0837638c30efe40
Johannes Natter <office@dsp-crowd.com>
2017-01-05 04:01:20 +0100

    rr_base: check root status in fpga-reconf.sh because as normal user export works but not changing the direction of the pin

--- raspberry_pi_zero/scripts/fpga-reconf.sh
+++ raspberry_pi_zero/scripts/fpga-reconf.sh
@@ -2,6 +2,11 @@
 
 pin_nr=18
 
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
 if [ ! -d "/sys/class/gpio/gpio${pin_nr}" ]; then
 	echo ${pin_nr} > /sys/class/gpio/export
 	echo out > /sys/class/gpio/gpio${pin_nr}/direction
