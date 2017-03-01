7d8ee30d604cc48ac00898301544d8a3c05cfeba
Johannes Natter <office@dsp-crowd.com>
2017-03-01 19:44:23 +0100

    rr_base: copy rbf to output directory

--- de0_nano/convert_rbf.sh
+++ de0_nano/convert_rbf.sh
@@ -2,5 +2,6 @@
 
 mkdir -p output
 
-python reverse_bits.py project/output_files/tbd_rr_base.rbf > output/rr_base.bin
+cp project/output_files/tbd_rr_base.rbf output/rr_base.rbf
+python reverse_bits.py output/rr_base.rbf > output/rr_base.bin
 md5sum output/rr_base.bin
