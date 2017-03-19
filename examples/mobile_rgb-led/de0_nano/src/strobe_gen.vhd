4d2e3828ee9e7ed976e338ff18e33448bfe5448e
Johannes Natter <office@dsp-crowd.com>
2017-03-20 00:55:09 +0100

    ex-rgb: add entity strobe generator (untested)

--- examples/mobile_rgb-led/de0_nano/src/strobe_gen.vhd
+++ examples/mobile_rgb-led/de0_nano/src/strobe_gen.vhd
@@ -1,15 +1,15 @@
 -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------
 --                                                                         --
---  This file is part of the DE0_Nano_Linux project                        --
---  http://www.de0nanolinux.com                                            --
+--  This file is part of the DSP-Crowd project                             --
+--  https://www.dsp-crowd.com                                              --
 --                                                                         --
 --  Author(s):                                                             --
---      - Helmut, redrocket@gmx.at                                         --
+--      - Johannes Natter, office@dsp-crowd.com                            --
 --                                                                         --
 -----------------------------------------------------------------------------
 --                                                                         --
---  Copyright (C) 2015 Authors and www.de0nanolinux.com                    --
+--  Copyright (C) 2015 Authors and www.dsp-crowd.com                       --
 --                                                                         --
 --  This program is free software: you can redistribute it and/or modify   --
 --  it under the terms of the GNU General Public License as published by   --
@@ -32,15 +32,60 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity strobe_gen is
+	generic
+	(
+		num_clock_cycles	: natural := 1
+	);
 	port
 	(
-		clock         : in  std_ulogic;
-		n_reset_async : in  std_ulogic;
-		strobe_output : out std_ulogic
+		clock			: in  std_ulogic;
+		n_reset_async		: in  std_ulogic;
+		strobe_output		: out std_ulogic
 	);
+begin
+	assert (num_clock_cycles >= 1)
+		report "strobe_gen: num_clock_cycles must be at least 1"
+		severity error;
 end strobe_gen;
 
 architecture rtl of strobe_gen is
+
+	type REG_TYPE is record
+		counter			: natural;
+	end record;
+
+	constant RSET_INIT_VAL : REG_TYPE :=
+	(
+		counter			=> 0
+	);
+
+	signal R, NxR			: REG_TYPE;
+
 begin
+
+	proc_comb: process(R)
+	begin
+
+		NxR <= R;
+
+		NxR.counter <= R.counter + 1;
+		strobe_output <= '0';
+
+		if(R.counter = num_clock_cycles - 1)then
+			NxR.counter <= 0;
+			strobe_output <= '1';
+		end if;
+
+	end process;
+
+	proc_reg: process(n_reset_async, clock)
+	begin
+		if(n_reset_async = '0')then
+			R <= RSET_INIT_VAL;
+		elsif(clock'event and clock = '1')then
+			R <= NxR;
+		end if;
+	end process;
+
 end architecture rtl;
 
