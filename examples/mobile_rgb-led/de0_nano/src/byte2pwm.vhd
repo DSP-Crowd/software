b62271fc8de86eb4faf84b72a10e25b830d265f5
Johannes Natter <office@dsp-crowd.com>
2017-04-06 22:22:26 +0200

    ex-rgb: Simplify module byte2pwm

--- examples/mobile_rgb-led/de0_nano/src/byte2pwm.vhd
+++ examples/mobile_rgb-led/de0_nano/src/byte2pwm.vhd
@@ -46,78 +46,42 @@ end byte2pwm;
 
 architecture rtl of byte2pwm is
 
-	type STATEMACHINE_STEP_TYPE is
-	(
-		SM_LOW, SM_HIGH
-	);
-
 	type REG_TYPE is record
-		sm_step			: STATEMACHINE_STEP_TYPE;
 		counter			: natural;
-		counter_buf		: natural;
 		counter_max		: natural;
 	end record;
 
 	constant RSET_INIT_VAL : REG_TYPE :=
 	(
-		sm_step			=> SM_LOW,
 		counter			=> 0,
-		counter_buf		=> 0,
 		counter_max		=> 0
 	);
 
 	signal R, NxR			: REG_TYPE;
-	signal sm_strobe		: std_ulogic;
+	signal cnt_strobe		: std_ulogic;
 
 begin
 
-	proc_comb: process(R, data, data_valid, sm_strobe)
+	proc_comb: process(R, data, data_valid, cnt_strobe)
 	begin
 		NxR <= R;
 
-		if(sm_strobe = '1')then
-			case R.sm_step is
-				when SM_LOW =>
-					if(R.counter = 253)then
-						NxR.counter <= 0;
-						if(R.counter_buf /= 0)then
-							NxR.counter_max <= R.counter_buf;
-							NxR.sm_step <= SM_HIGH;
-						end if;
-					else
-						NxR.counter <= R.counter + 1;
-					end if;
-
-				when SM_HIGH =>
-					if(R.counter = R.counter_max - 1)then
-						NxR.sm_step <= SM_LOW;
-					else
-						NxR.counter <= R.counter + 1;
-					end if;
-
-					if(R.counter = 254)then
-						NxR.counter <= 0;
-						if(R.counter_buf /= 0)then
-							NxR.counter_max <= R.counter_buf;
-							NxR.sm_step <= SM_HIGH;
-						else
-							NxR.sm_step <= SM_LOW;
-						end if;
-					end if;
-
-				when others =>
-					NxR.sm_step <= SM_LOW;
-			end case;
+		if(cnt_strobe = '1')then
+			if(R.counter < 254)then
+				NxR.counter <= R.counter + 1;
+			else
+				NxR.counter <= 0;
+			end if;
 		end if;
 
-		if(R.sm_step = SM_HIGH)then
+		if(R.counter < R.counter_max)then
 			led_pwm <= '1';
 		else
 			led_pwm <= '0';
 		end if;
 
 		if(data_valid = '1')then
-			NxR.counter_buf <= to_integer(unsigned(data));
+			NxR.counter_max <= to_integer(unsigned(data));
 		end if;
 
 	end process;
@@ -132,7 +96,7 @@ begin
 	(
 		clock		=> clock,
 		n_reset_async	=> n_reset_async,
-		strobe_output	=> sm_strobe
+		strobe_output	=> cnt_strobe
 	);
 
 	proc_reg: process(n_reset_async, clock)
