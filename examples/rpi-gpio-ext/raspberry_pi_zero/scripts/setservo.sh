efb5e6cf56fe49ef37bf9c63dc072c9479782f4a
Johannes Natter <office@dsp-crowd.com>
2017-04-19 22:52:09 +0000

    ex-gpio: Add script setservo.sh

---
+++ examples/rpi-gpio-ext/raspberry_pi_zero/scripts/setservo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+id=$1
+val3=${2:0:2}
+val2=${2:2:2}
+val1=${2:4:2}
+val0=${2:6:2}
+
+#echo "Servo ID: $id"
+#echo "Value: $val3 $val2 $val1 $val0"
+
+echo -ne "\x$id\x02\x00\x0F\x42\x40\x$val3\x$val2\x$val1\x$val0" > /dev/spidev0.3
