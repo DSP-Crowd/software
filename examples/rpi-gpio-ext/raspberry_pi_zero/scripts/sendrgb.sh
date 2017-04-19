367ec099c854a79d61941e435e6ff73560547192
Johannes Natter <office@dsp-crowd.com>
2017-04-19 21:13:16 +0000

    ex-gpio: Update sendrgb script for new vhdl layer

--- examples/rpi-gpio-ext/raspberry_pi_zero/scripts/sendrgb.sh
+++ examples/rpi-gpio-ext/raspberry_pi_zero/scripts/sendrgb.sh
@@ -4,4 +4,8 @@ red=${1:0:2}
 green=${1:2:2}
 blue=${1:4:2}
 
-echo -ne "\x00\x00\x$red\x$green\x$blue" > /dev/spidev0.3
+#echo "Color: $red $green $blue"
+
+echo -ne "\x3F\x02\x00\x00\x00\xFF\x00\x00\x00\x$red" > /dev/spidev0.3
+echo -ne "\x3D\x02\x00\x00\x00\xFF\x00\x00\x00\x$green" > /dev/spidev0.3
+echo -ne "\x3C\x02\x00\x00\x00\xFF\x00\x00\x00\x$blue" > /dev/spidev0.3
