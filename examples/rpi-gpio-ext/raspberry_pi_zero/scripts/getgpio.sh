29c1ff99e97bee73dbcb84394b0f25f8ec3772cb
Johannes Natter <office@dsp-crowd.com>
2017-04-20 00:34:47 +0000

    ex-gpio: Use dirname in script getgpio.sh

--- examples/rpi-gpio-ext/raspberry_pi_zero/scripts/getgpio.sh
+++ examples/rpi-gpio-ext/raspberry_pi_zero/scripts/getgpio.sh
@@ -4,10 +4,13 @@ id=$1
 
 #echo "GPIO ID: $id"
 
-valfull=$(python spiarray.py $id 00 00)
+script=$(dirname $0)/spiarray.py
+#echo "Script: $script"
+
+valfull=$(python $script $id 00 00)
 val=${valfull:5:1}
 
 #echo "Value full: $valfull"
 #echo "Value:      $val"
 
-echo $val
+echo "$val"
