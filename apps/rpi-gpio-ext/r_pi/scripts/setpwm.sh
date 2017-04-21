a36996fdcf4c3bb33640e03728d1444a3900f6f6
Johannes Natter <office@dsp-crowd.com>
2017-04-21 15:49:14 +0000

    ex-gpio: Update script setpwm.sh to new API

--- apps/rpi-gpio-ext/r_pi/scripts/setpwm.sh
+++ apps/rpi-gpio-ext/r_pi/scripts/setpwm.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-id=$1
+id=$(printf "%02x" $1)
 
 # pv = period value
-pv3=${2:0:2}
-pv2=${2:2:2}
-pv1=${2:4:2}
-pv0=${2:6:2}
+phex=$(printf "%08x" $2)
+pv3=${phex:0:2}
+pv2=${phex:2:2}
+pv1=${phex:4:2}
+pv0=${phex:6:2}
 
 # dv = duty cycle value
-dv3=${3:0:2}
-dv2=${3:2:2}
-dv1=${3:4:2}
-dv0=${3:6:2}
+dhex=$(printf "%08x" $3)
+dv3=${dhex:0:2}
+dv2=${dhex:2:2}
+dv1=${dhex:4:2}
+dv0=${dhex:6:2}
 
 echo -ne "\x$id\x02\x$pv3\x$pv2\x$pv1\x$pv0\x$dv3\x$dv2\x$dv1\x$dv0" > /dev/spidev0.3
 #echo "cmd: $id 02 $pv3 $pv2 $pv1 $pv0 $dv3 $dv2 $dv1 $dv0"
